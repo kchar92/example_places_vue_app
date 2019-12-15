bc34806a29018af5882a9218f56f480585dc9ee0
Keelana Char <keelanachar@gmail.com>
2019-12-15 12:33:48 -0600

    create index and new/create actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
+    <p>Name: <input type="text" v-model="newName"></p>
+    <p>Address: <input type="text" v-model="newAddress"></p>
+    <button v-on:click="addPlace()">Add Place</button>
+
+
+    <div v-for="place in places">
+      <p>Name: {{ place.name }}</p>
+      <p>Address: {{ place.address }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [],
+      newName: "",
+      newAddress: ""
+    };
+  },
+  created: function() {
+    console.log("in created");
+    axios.get("/api/places").then(response => {
+      console.log(response.data);
+      this.places = response.data;
+    });
+  },
+  methods: {
+    addPlace: function() {
+      console.log("adding place");
+
+      var params = {
+        name: this.newName,
+        address: this.newAddress
+      };
+
+      axios.post("/api/places", params).then(response => {
+        console.log(response.data);
+        this.newName = "";
+        this.newAddress = "";
+      });
+    }
   }
-}
+};
 </script>
+
